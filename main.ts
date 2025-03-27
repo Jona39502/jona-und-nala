c8c2d86ae9f3d9a8d5642b3f7c9ff02fc70f8ce6
Jona39502 <jona.svh@web.de>
2025-03-27 18:49:50 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,45 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    music.stopAllSounds()
+    zeit += -1
+    countdown += -1
+    basic.showString("" + (zeit))
+})
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+    music.stopMelody(MelodyStopOptions.All)
+    ende = 1
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    zeit += 1
+    countdown += 1
+    basic.showString("" + (zeit))
 })
 input.onGesture(Gesture.Shake, function () {
-    basic.pause(30000)
-    music.playMelody("C5 B C5 B C5 B C5 B ", 120)
+    for (let index = 0; index < zeit + 1; index++) {
+        basic.showNumber(countdown)
+        countdown += -1
+    }
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . . # . .
+        `)
+    countdown = zeit
+    while (ende == 0) {
+        music.playMelody("C5 B C5 B C5 B C5 B ", 150)
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    ende = 0
 })
-basic.forever(function () {
-	
-})
+let ende = 0
+let countdown = 0
+let zeit = 0
+zeit = 5
+countdown = 5
+ende = 0
